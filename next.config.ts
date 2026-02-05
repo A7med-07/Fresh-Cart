2e67a61dbf1a11ff3a1595b6c7b10b2d740cf38e
A7med-07 <ahmed.0.7sabry.0.5@gmail.com>
2026-02-05 15:27:15 +0200

    next commit

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,25 @@
 import type { NextConfig } from "next";
 
-// https://ecommerce.routemisr.com/Route-Academy-products/1680403397402-cover.jpeg
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  // عشان NextAuth يشتغل صح على Production
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+  
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'ecommerce.routemisr.com',
         pathname: '/*/**',
-        
       },
     ],
   },
 };
 
 export default nextConfig;
+
+
+ // https://ecommerce.routemisr.com/Route-Academy-products/1680403397402-cover.jpeg
